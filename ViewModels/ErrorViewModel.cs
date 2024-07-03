b142553df2b04d0f9391af01ecb03fc5fff0a8db
Wooboy <github@wooboy.me>
2024-07-03 16:41:20 +0800

    deploy controller

---
+++ ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RentalManagement.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
