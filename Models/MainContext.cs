ee7fa531c4f00b56bb2a7a1b89b36a4977f66d16
Wooboy <github@wooboy.me>
2024-07-04 18:07:55 +0800

    add: db connection

--- Models/MainContext.cs
+++ Models/MainContext.cs
@@ -7,10 +7,8 @@ public class MainContext : DbContext
 {
     public MainContext() { }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
-        options.UseSqlServer(
-            "server=cyldbms1.cylee.com;initial catalog=WBTEST;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true;Encrypt=False"
-        );
+    public MainContext(DbContextOptions<MainContext> options)
+        : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -20,25 +18,28 @@ public class MainContext : DbContext
             .WithOne(e => e.Client)
             .HasForeignKey(e => e.ClientId)
             .IsRequired(false);
+        modelBuilder.Entity<Property>().HasMany(e => e.Contracts).WithMany(e => e.Properties);
         modelBuilder
             .Entity<Property>()
-            .HasMany(e => e.Contracts)
-            .WithMany(e => e.Properties);
+            .HasData(
+                new Property
+                {
+                    Id = 1,
+                    Name = "Property 1",
+                    Location = "Location 1"
+                }
+            );
         modelBuilder
             .Entity<Property>()
-            .HasData(new Property{
-                 Id = 1,
-                 Name = "Property 1",
-                 Location = "Location 1"
-            });
-        modelBuilder
-            .Entity<Property>()
-            .HasData(new Property{
-                 Id = 2,
-                 Name = "Property 11",
-                 Location = "Location 1",
-                 ParentPropertyId = 1
-            });
+            .HasData(
+                new Property
+                {
+                    Id = 2,
+                    Name = "Property 11",
+                    Location = "Location 1",
+                    ParentPropertyId = 1
+                }
+            );
     }
 
     public DbSet<SYS_User> SYS_User { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using RentalManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddDbContext<MainContext>(
+    options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("MainContext")
+                ?? throw new InvalidOperationException("Connection string 'MainContext' not found.")
+        )
+);
 
+var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
@@ -17,8 +27,6 @@ app.UseRouting();
 
 //app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
